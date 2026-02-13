38bd800950e5d2533ed52155e18abdd4a7f2decb
Stanley <Ryan.Stanley@dfo-mpo.gc.ca>
2026-02-13 15:13:26 -0400

    code tweaks, fixed dependencies.

--- code/marmap_to_isobath.R
+++ code/marmap_to_isobath.R
@@ -1,17 +1,16 @@
-marmap_to_isobath <- function(marmap,depth= -200,proj=NULL){
+marmap_to_isobath <- function(marmap_df,depth= -200,proj=NULL){
 
   #marmap - this is a call from the getNOAA.bathy in marmap
   #depth - this is a value for the isobath. Default is 200m. 
   #proj - this is the projection you want returned 
     
   require(terra)
-  require(abind)
-  
+  require(sf)
 
   if(is.null(proj)){message("No projection specified. Default to a NAD83 unprojected coordinate system.")}
   if(is.null(proj)){proj <- "+proj=longlat +datum=NAD83 +no_defs +ellps=GRS80 +towgs84=0,0,0"}
           
-  out <- marmap%>%
+  out <- marmap_df%>%
          marmap::as.raster(.)%>%
          terra::rast(.)%>%
          as.contour(.,levels = depth)%>%
