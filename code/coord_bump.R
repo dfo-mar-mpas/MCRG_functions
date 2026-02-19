bba92dd67cad63ab47ae7e0e20e2560a29459fde
Stanley <Ryan.Stanley@dfo-mpo.gc.ca>
2026-02-19 14:40:39 -0400

    functions for handling least-cost-path analyses using basemaps from RNatural earth as the land 'costs'

--- code/coord_bump.R
+++ code/coord_bump.R
@@ -1,5 +1,5 @@
-
-coord_bump <- function(x,ras,radius = 10){ #code to identify the nearest 'in ocean' location to a coordinate that is not in the ocean
+##First order adjustment function function 'coord_bump' ---------------------
+coord_bump <- function(x,ras,radius = 10){
   
   require(dplyr)
   require(sf)
@@ -12,7 +12,7 @@ coord_bump <- function(x,ras,radius = 10){ #code to identify the nearest 'in oce
  
   rasproj <- proj4string(ras)
   
-  x <- st_as_sf(x,coords=c("long","lat"),crs=rasproj) #convert to sf
+  x <- st_as_sf(x,coords=c("lon","lat"),crs=rasproj) #convert to sf
   x2 <- x%>%st_transform("+proj=longlat +datum=NAD83 +no_defs +ellps=GRS80 +towgs84=0,0,0")
   
   #set up a dynamic azimutal equidistance projection for each point 
@@ -26,31 +26,37 @@ coord_bump <- function(x,ras,radius = 10){ #code to identify the nearest 'in oce
         st_buffer(radius*1000)%>% # km search radius for water
         st_as_sf()%>%
         st_transform(proj4string(ras))%>% #Back to the projection of the raster
-        extent()
+        raster::extent()
 
   bathy <- crop(ras,cr)
   
-  #this is a logical to catch scenarios in the present climate where the rasters have 0 instead of NAs for land
-  if(sum(is.na(values(bathy))) == 0){values(bathy)[values(bathy) == 0] <- NA}
-  
+  bathy[values(bathy)>0] <- NA
+
   xy <- st_coordinates(x)%>%SpatialPoints()
   
   depth <- raster::extract(bathy,xy)
 
-  if(is.na(depth)){
+  if (is.na(depth)) {
     
-    if(sum(!is.na(values(bathy)))>1){
-         bathypoints <- coordinates(bathy)[!is.na(values(bathy)),]%>%
-                        data.frame()%>%
-                        st_as_sf(coords=c("x","y"),crs=rasproj)
-    }else{bathypoints <- data.frame(x=coordinates(bathy)[!is.na(values(bathy)),][1],
-                                    y=coordinates(bathy)[!is.na(values(bathy)),][2])%>%
-                         st_as_sf(coords=c("x","y"),crs=rasproj)}
-                  
-  nearest_point <- bathypoints[suppressMessages(st_nn(x,bathypoints,progress = FALSE))[[1]],]
+    if (sum(!is.na(values(bathy))) > 1) {
+      bathypoints <- coordinates(bathy)[!is.na(values(bathy)), ] %>%
+        data.frame() %>%
+        st_as_sf(coords = c("x", "y"), crs = rasproj)
+    } else {
+      bathypoints <- data.frame(
+        x = raster::coordinates(bathy)[!is.na(values(bathy)), ][1],
+        y = raster::coordinates(bathy)[!is.na(values(bathy)), ][2]
+      ) %>%
+        st_as_sf(coords = c("x", "y"), crs = rasproj)
+    }   # <-- this closes the else
   
-  return(st_coordinates(nearest_point)%>%data.frame()%>%rename(long=X,lat=Y))
+    
+    nearest_point <- bathypoints[suppressMessages(st_nn(x%>%st_transform(rasproj),bathypoints%>%st_transform(rasproj),progress = FALSE))[[1]],]
+    
+    #now return outputs
+    
+    return(st_coordinates(nearest_point)%>%data.frame()%>%rename(lon_a=X,lat_a=Y))
+    
+  } else {return(st_coordinates(x)%>%data.frame()%>%rename(lon_a=X,lat_a=Y))}
   
-  } else {return(st_coordinates(x)%>%data.frame()%>%rename(long=X,lat=Y))}
-      
 }
