f32f8f50a77cc82adcbc9fd338649ecbe2535b6c
Stanley <Ryan.Stanley@dfo-mpo.gc.ca>
2026-04-10 11:35:43 -0300

    added the reverse

--- code/ddm_to_dd.R
+++ code/ddm_to_dd.R
@@ -1,12 +1,22 @@
 #Convert degress decimal minutes to decimal degress
 
-ddm_to_dd <- function(ddm_string) {
-  # Split into degrees and decimal minutes
-  parts <- as.numeric(unlist(strsplit(as.character(ddm_string), " ")))
-  degrees <- parts[1]
-  decimal_minutes <- parts[2]
-  
-  # Convert to decimal degrees
-  dd <- degrees + (decimal_minutes / 60)
-  return(dd)
+ddm_to_dd <- function(value, reverse = FALSE) {
+  if (!reverse) {
+    # Convert from DDM to DD
+    parts <- as.numeric(unlist(strsplit(as.character(value), " ")))
+    degrees <- parts[1]
+    decimal_minutes <- parts[2]
+    
+    # Convert to decimal degrees
+    dd <- degrees + (decimal_minutes / 60)
+    return(dd)
+  } else {
+    # Convert from DD to DDM
+    dd <- as.numeric(value)
+    degrees <- floor(dd)
+    decimal_minutes <- (dd - degrees) * 60
+    
+    # Return as formatted string
+    return(paste(degrees, round(decimal_minutes, 4)))
+  }
 }
